6f02f605a0e3b17aa863d1586ea890effcbbe695
yashadmane95 <156408337+yashadmane95@users.noreply.github.com>
2026-09-08 01:56:05 +0530

    Update Spotify SQL project with new queries and structure
    Refactor SQL queries and create table structure for Spotify database. Added various data analysis queries and optimizations.

--- Spotify project.sql
+++ Spotify project.sql
@@ -187,6 +187,23 @@ Advanced Level
 Find the top 3 most-viewed tracks for each artist using window functions.
 Write a query to find tracks where the liveness score is above the average.
 Use a WITH clause to calculate the difference between the highest and lowest energy values for tracks in each album.
+'''sql
+
+	WITH ranking_artist
+AS
+(SELECT
+    artist,
+	track,
+	SUM(views) as total_view,
+	DENSE_RANK() OVER(PARTITION BY artist ORDER BY SUM(views) DESC) as rank
+FROM spotify
+GROUP BY 1, 2
+ORDER BY 1, 3 DESC
+)
+SELECT * FROM ranking_artist
+WHERE rank <=3
+	
+'''
 Find tracks where the energy-to-liveness ratio is greater than 1.2.
 Calculate the cumulative sum of likes for tracks ordered by the number of views, using window functions.
 */
